69f756fd6ef5fccd9aa2bfe80f934d6cf31b708b
a.gordynets <a.gordynets@godeltech.com>
2018-05-08 17:54:24 +0300

    -commit webDriver

--- src/main/java/com/browser/CustomBrowser.java
+++ src/main/java/com/browser/CustomBrowser.java
@@ -1,4 +1,24 @@
 package com.browser;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CustomBrowser {
+
+    private static final String HUB_PROPERTIES_PATH = System.getProperty("hubProperties");
+
+    private ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
+
+    public WebDriver getDriver() {
+        String driverType = System.getProperty("driver.type");
+        WebDriver driver;
+        switch (driverType) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            default:
+                driver = new ChromeDriver();
+        }
+        return driver;
+    }
 }
--- src/test/java/com/google/search/SearchGoogleTest.java
+++ src/test/java/com/google/search/SearchGoogleTest.java
@@ -1,4 +1,33 @@
 package com.google.search;
 
+import com.browser.CustomBrowser;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class SearchGoogleTest {
+    private WebDriver driver;
+
+    @BeforeClass()
+    public void beforeClass() {
+        CustomBrowser cb = new CustomBrowser();
+        driver = cb.getDriver();
+    }
+
+
+    @Test()
+    public void testGoogleSearchPage() {
+        driver.navigate().to("https://www.google.es/");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterClass()
+    public void closeBrowser() {
+        driver.close();
+    }
 }
